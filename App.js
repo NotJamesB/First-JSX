c31defea9c98c0b54c962f648efb616b4927a3e2
NotJamesB <97855508+NotJamesB@users.noreply.github.com>
2022-07-08 12:04:59 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="FirstJSX">
+      <h1 className="dh">Hello Dojo!</h1>
+      <p><h3>Things i need to do:</h3></p>
+      <p>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default App;
